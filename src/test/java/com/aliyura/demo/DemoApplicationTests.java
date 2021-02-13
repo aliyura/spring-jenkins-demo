8f43c80d9f7073740fba606d8892aa363862b1bf
Rabiu Aliyu <net.rabiualiyu@gmail.com>
2021-02-13 08:32:39 +0100

    Fixes

---
+++ src/test/java/com/aliyura/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.aliyura.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootConfiguration
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
